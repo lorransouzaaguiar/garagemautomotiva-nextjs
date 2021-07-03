be2c403ded0a450b3d47be85f00e374578489e16
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2021-07-03 19:27:24 -0300

    fixed some bugs with eslint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import {Text} from '@chakra-ui/react'
+import React from 'react';
+
+import { Text } from '@chakra-ui/react';
+
+const object = {
+  name: '',
+  jose: '',
+};
 
 export const Header: React.FC = () => {
-    return (
-        <Text id='1'>Ola</Text>
-    )
-}
-
-
+  return <Text id="1">Ola</Text>;
+};
