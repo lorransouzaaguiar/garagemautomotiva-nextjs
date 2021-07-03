69b5e27239f0e6187b3d38827dd33d8129224f43
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2021-07-03 00:23:56 -0300

    nextjs-template-project

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Flex, Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
+
+import { Header } from '~/components/Header';
+
+const PageComponent: NextPage = () => {
+    return (
+        <Flex direction="column">
+            <Header />
+            <Flex>
+                <Text>Home</Text>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default PageComponent;
