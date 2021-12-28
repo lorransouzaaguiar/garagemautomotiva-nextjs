24d2fc356d3bdbe48bcec67ebc359098a2d73026
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2021-12-28 12:30:09 -0300

    change CustomerValidator schema to static

--- src/app/modules/customer/presentation/CustomerValidator.ts
+++ src/app/modules/customer/presentation/CustomerValidator.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 export class CustomerValidator {
 
-    readonly schema = yup.object().shape({
+    static readonly schema = yup.object().shape({
         name: yup.string().required('O nome é obrigatório'),
         number: yup.string().required('O número é obrigatório'),
         email: yup.string().email('O email precisa ser válido').required('O email é obrigatório')
